41573e62cf613c8669908ee2df2a8de4bd0ce70a
ChoiSol <choisoloa@ChoiSolui-MacBookPro.local>
2026-01-06 09:29:48 +0900

    Add GitHub Packages deployment workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,12 +36,24 @@ tasks {
 
 publishing {
     publications {
-        create<MavenPublication>("shadow") {
+        create<MavenPublication>("gpr") {
+            // ShadowJar를 배포하도록 설정
             project.shadow.component(this)
 
-            groupId = "dev.hiroine"
+            groupId = "dev.hiro"
             artifactId = "flux-api"
             version = "1.0.0"
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            // 사용자명/리포지토리명 경로를 정확히 입력하세요
+            url = uri("https://maven.pkg.github.com/사용자명/FluxAPI")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR") // 실행하는 유저 이름 자동 인식
+                password = System.getenv("GITHUB_TOKEN") // 실행 환경의 토큰 자동 인식
+            }
         }
     }
 }
