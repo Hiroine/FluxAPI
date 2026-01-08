3a5cac54274385529ded6811a959f774cb5f6f6e
hiroine <moongw0813@naver.com>
2026-01-08 16:48:48 +0900

    Desktop Commit Flux-API
    Test Desktop GitHub Commit

--- src/main/kotlin/dev/hiroine/flux/plugin/FluxAPI.kt
+++ src/main/kotlin/dev/hiroine/flux/plugin/FluxAPI.kt
@@ -1,6 +1,11 @@
 package dev.hiroine.flux.plugin
 
+import dev.hiroine.flux.chat.send
+import dev.hiroine.flux.kommand.kommand
 import dev.hiroine.flux.ui.event.UIGlobalListener
+import dev.hiroine.flux.ui.uiFx
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
 
 class FluxAPI : JavaPlugin() {
